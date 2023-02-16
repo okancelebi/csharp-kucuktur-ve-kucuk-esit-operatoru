7cbb7bc10587921da97941332a5116644939aca7
Okan Çelebi <okancelebi06@gmail.com>
2023-02-16 22:58:51 +0300

    Add files via upload

---
+++ csharp kucuktur ve kucuk esit operatoru/csharp kucuktur ve kucuk esit operatoru/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_kucuktur_ve_kucuk_esit_operatoru
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Küçüktür operatörü : <
+            //Küçük eşit operatörü : <=
+            int sayi1 = 5;
+            bool kontrolet = sayi1 < 4;
+            int sayi2 = 4;
+            bool kontrolet2 = sayi2 <= 4;
+            Console.WriteLine(kontrolet);
+            Console.WriteLine(kontrolet2);
+            Console.ReadLine();
+        }
+    }
+}
